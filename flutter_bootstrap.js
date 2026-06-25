a12e0740cc64afbb9c9e833c9dfbe4a14ea3c605
Daniel George <danielsgeorge@gmail.com>
2026-06-24 18:30:30 -0600

    Deploy to github pages (main@1429008)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4188389532" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2078659859" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
